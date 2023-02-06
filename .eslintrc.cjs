bc7d2e237cfae5e3da34f0fb8a2e782e7c6a7584
Rainbow <jscrazy@yandex.com>
2023-02-06 09:58:38 +0800

    error

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,7 +17,7 @@ module.exports = {
     'parserOptions': {
         'ecmaVersion': 'latest',
         'sourceType': 'module',
-        'project': './tsconfig.json',
+        // 'project': './tsconfig.json',
     },
     'plugins': [
         'react',
